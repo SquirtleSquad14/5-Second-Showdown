905c8d5b2be70ccc192d7c5f82643f474fef710f
Michael Angelo <13angelomichael@gmail.com>
2023-04-04 16:10:13 -0500

    added proxy for assets

--- app/server/server.ts
+++ app/server/server.ts
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, '../client/assets')))
+app.use(express.static(path.resolve(__dirname, '../client')))
 
 // ---------------------ROUTES---------------------
 
