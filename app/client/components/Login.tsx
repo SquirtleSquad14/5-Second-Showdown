39429cacd8229772699cea1aa443bbca2f729574
brianlim89 <br.lim89@gmail.com>
2023-04-04 23:07:38 -0700

    Reworked oauth

--- app/client/components/Login.tsx
+++ app/client/components/Login.tsx
@@ -1,18 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
-import Oauth from "../Oauth";
+import Oauth from "./Oauth";
 import { googleLogout } from "@react-oauth/google";
-import "../styles/login.css"
+import "../styles/login.css";
 
-type Props = {
-
-};
+type Props = {};
 
 const Login = ({}: Props): JSX.Element => {
-    const [userID, setUserID] = useState<any>();
-    // const [username, setUsername] = useState();
-    // const [password, setPassword] = useState();
+  const [userID, setUserID] = useState<any>();
+  // const [username, setUsername] = useState();
+  // const [password, setPassword] = useState();
 
   return (
     <div>
@@ -20,14 +18,14 @@ const Login = ({}: Props): JSX.Element => {
         <h1 id="login-banner">5 Second Showdown Login</h1>
       </div>
       <div className="form-container">
-        <form id='input-form' action='/login' method='POST' >
+        <form id="input-form" action="/login" method="POST">
           <div>
-            <input type="text" id="uname" placeholder="Username"></input> 
+            <input type="text" id="uname" placeholder="Username"></input>
           </div>
           <div>
             <input type="text" id="pword" placeholder="Password"></input>
           </div>
-          <div className= "choose-btns">
+          <div className="choose-btns">
             <button id="submit-btn">Submit</button>
             {/* <input type="button" id="submit-btn" value="Submit" />
             <input type="button" id="signup-btn" value="Sign Up" /> */}
@@ -35,15 +33,17 @@ const Login = ({}: Props): JSX.Element => {
         </form>
         <div className="google-btn">
           <Oauth setUserID={setUserID} />
-       </div>
-       <div>
-          <button id="signup-btn"><Link id="signup-btn" to='/signup'>Sign Up</Link></button>
-       </div>
+        </div>
+        <div>
+          <button id="signup-btn">
+            <Link id="signup-btn" to="/signup">
+              Sign Up
+            </Link>
+          </button>
+        </div>
       </div>
-
     </div>
-  )
-    
-}
+  );
+};
 
 export default Login;
